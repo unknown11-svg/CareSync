6819724530fd3ca0a51548c29bccf3b2d5961c67
unknown11-svg <2434088@students.wits.ac.za>
2025-09-05 12:46:40 +0200

    Triggering redeployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://caresync-gdevh3eccggqhjch.southafricanorth-01.azurewebsites.net/api',
   timeout: 10000,
 });
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://caresync-gdevh3eccggqhjch.southafricanorth-01.azurewebsites.net/',
+        target: 'http://localhost:5000',
         changeOrigin: true
       }
     }
