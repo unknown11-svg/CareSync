7243db1c8af13943bfc6a4640fc4fc3c0ae3a171
unknown11-svg <2434088@students.wits.ac.za>
2025-09-05 12:31:57 +0200

    Update API proxy target in Vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://render.com/docs/web-services#port-binding',
+        target: 'https://caresync-gdevh3eccggqhjch.southafricanorth-01.azurewebsites.net/',
         changeOrigin: true
       }
     }
